6c4072e48b86a7f9c9e446950f4ede7e34d18dea
Konstantin-Basalov <konstantin-basalov@yandex.ru>
2021-03-25 16:14:46 +0300

    webpack config & start app

---
+++ src/header.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export function Header() {
+    return (
+        <div>
+            <h1>SkillBox Course "React"</h1>
+            <p>тут будет текст, но потом</p>
+        </div>
+    )
+}
+
