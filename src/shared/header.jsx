0e3eca51aa648a900de6c96171aee7d0c380a944
Konstantin-Basalov <konstantin-basalov@yandex.ru>
2021-04-04 21:23:31 +0300

    home work 2.6

--- src/shared/header.jsx
+++ src/shared/header.jsx
@@ -5,8 +5,8 @@ import styles from './header.css';
 function HeaderComponent() {
     return (
         <div>
-            <h1 className={styles.example}>SkillBox Course "React"</h1>
-            <p className={styles.style__second}>New text here</p>
+            <h1 className={styles.example}>Hello React</h1>
+            <p className={styles.style__second}>Home Work</p>
         </div>
 
     )
